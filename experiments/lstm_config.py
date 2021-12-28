7fc8b01d6a7322b3e38d1655e849d00402693beb
HHansi <h.hansi1991@gmail.com>
2021-12-28 14:43:59 +0000

    003: Updated configs

--- experiments/lstm_config.py
+++ experiments/lstm_config.py
@@ -18,7 +18,7 @@ config = {
     'max_features': None,  # how many unique words to use (i.e num rows in embedding vector)
     'batch_size': 64,  # 64, 8
     'n_epochs': 20,
-    'earlystopping_patience': 2,
+    'earlystopping_patience': 5,  # 2
     'prediction_batch_size': 64,
 
     'train_file': 'train.tsv',
@@ -30,6 +30,9 @@ config = {
 
     'label_list': ['positive', 'negative', 'neutral'],
 
-    'embedding_details': {'glove': 'F:/workspace-backup/Models/glove.840B.300d/glove.840B.300d.txt',
-                          'fasttext': 'F:/workspace-backup/Models/crawl-300d-2M-subword/crawl-300d-2M-subword.vec'}
+    # 'embedding_details': {'glove': 'F:/workspace-backup/Models/glove.840B.300d/glove.840B.300d.txt',
+    #                       'fasttext': 'F:/workspace-backup/Models/crawl-300d-2M-subword/crawl-300d-2M-subword.vec'},
+    'embedding_details': {'glove': '/content/glove.840B.300d/glove.840B.300d.txt',
+                          'fasttext': '/content/crawl-300d-2M-subword/crawl-300d-2M-subword.vec'},
+
 }
