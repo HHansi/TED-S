ab260f6c3846f856922b5fdcebe6ed4187f3de97
HHansi <h.hansi1991@gmail.com>
2022-01-03 17:41:34 +0000

    006: Refactored algo

--- algo/util/label_encoder.py
+++ algo/util/label_encoder.py
@@ -9,19 +9,10 @@ def encode(df, label_column='label'):
     # df[label_column] = df[label_column].replace('neutral', label_mapping['neutral'])
     # df[label_column] = df[label_column].replace('positive', label_mapping['positive'])
 
-    # df[label_column].replace('negative', label_mapping['negative'], inplace=True)
-    # df[label_column].replace('neutral', label_mapping['neutral'], inplace=True)
-    # df[label_column].replace('positive', label_mapping['positive'], inplace=True)
-
     df = df.copy()
-
     df.loc[df[label_column] == 'negative', label_column] = label_mapping['negative']
     df.loc[df[label_column] == 'neutral', label_column] = label_mapping['neutral']
     df.loc[df[label_column] == 'positive', label_column] = label_mapping['positive']
-
-    # df.loc[df[label_column] == 'neutral'] = label_mapping['neutral']
-    # df.loc[df[label_column] == 'positive'] = label_mapping['positive']
-
     return df
 
 
