accd80c4675f615163f36445dab9487bd7c0d32c
HHansi <h.hansi1991@gmail.com>
2022-01-05 11:03:02 +0000

    003: Updated configs

--- experiments/transformer_config.py
+++ experiments/transformer_config.py
@@ -21,7 +21,7 @@ config = {
 
     'fp16': False,
     'fp16_opt_level': 'O1',
-    'max_seq_length': 84,  # 96
+    'max_seq_length': 96,  # 96
     'train_batch_size': 16,  # 8
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 16,  # 8
@@ -32,7 +32,7 @@ config = {
     'warmup_ratio': 0.06,
     'warmup_steps': 0,
     'max_grad_norm': 1.0,
-    'do_lower_case': True,
+    'do_lower_case': False,
 
     'logging_steps': 100,  #40,
     'save_steps': 100,  #40,
